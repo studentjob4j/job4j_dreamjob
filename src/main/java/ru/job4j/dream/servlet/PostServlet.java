3b19cf27727f90a434837ba0ee70c0592cfb62ac
Evgenii <job4jstudy@yandex.ru>
2021-06-25 17:19:31 +1000

    Servlet. Web.xml

---
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -0,0 +1,31 @@
+package ru.job4j.dream.servlet;
+
+/**
+ * @author Evgenii Shegai
+ * @since 25.06.2021
+ * @version 1.0
+ * Принцип работы - выводится в браузере файл index.jsp - далее newVacancy.jsp - вводим имя и описание новой заявки
+ * - в методе PortServlet создается новая заявка сохраняется в хранилище далее создаем еще один запрос к файлу post.jsp
+ * получаем все заявки из хранилища
+ */
+
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.Store;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet(name = "PostServlet", value = "/PostServlet")
+public class PostServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String[] answer = request.getParameterValues("name");
+        Store.instOf().save(new Post(answer[0], 0, answer[1], LocalDate.now()));
+        response.sendRedirect(request.getContextPath() + "/posts.jsp");
+    }
+
+}
--- src/main/webapp/newVacancy.jsp
+++ src/main/webapp/newVacancy.jsp
@@ -32,13 +32,15 @@
     <div class="row">
         <div class="card" style="width: 100%">
             <div class="card-header">
-                Новая вакансия.
+                Новая вакансия. Введите имя и описание
             </div>
             <div class="card-body">
-                <form>
+                <form action="<%=request.getContextPath()%>/post/save" method="post">
                     <div class="form-group">
                         <label>Имя</label>
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="name">
+                        <label>Описание</label>
+                        <input type="text" class="form-control" name="name">
                     </div>
                     <button type="submit" class="btn btn-primary">Сохранить</button>
                 </form>
