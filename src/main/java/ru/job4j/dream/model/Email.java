ab780ac82be039c01b8c558fb99c8901164873f8
Evgenii <job4jstudy@yandex.ru>
2022-01-25 05:02:51 +1000

    JSON формат

---
+++ src/main/java/ru/job4j/dream/model/Email.java
@@ -0,0 +1,21 @@
+package ru.job4j.dream.model;
+
+/**
+ * @author Shegai Evgenii
+ * @since 25.01.20222
+ * @version 1.0
+ */
+
+
+public class Email {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,22 +1,52 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.job4j.dream.model.Email;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author Shegai Evgenii
+ * @since 25.01.20222
+ * @version 1.0
+ * В случае ошибки CROSS error добавил resp.setHeader("Access-Control-Allow-Origin", "*"); в GET
+ * и crossdomain: true, в javascript  на странице ajax.html
+ */
 
 public class GreetingServlet extends HttpServlet {
 
+    private final List<Email> emails = new CopyOnWriteArrayList<>();
+    private static final Gson GSON = new GsonBuilder().create();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Email email = GSON.fromJson(req.getReader(), Email.class);
+        emails.add(email);
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(email);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setHeader("Access-Control-Allow-Origin", "*");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(emails);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
     }
 }
